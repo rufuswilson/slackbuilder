8a0dd1d9ebef5a6643a1a0efb82db35689fff5f4
rufuswilson <marsou75@gmail.com>
2025-06-10 21:52:04 +0200

    Added more dependency package types for installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 cd /build-deps
 echo
 echo '#####  Installing Packages  #####'
-find /build-deps -name "*.txz" -exec bash -c 'echo Installing "{}" && installpkg "{}" 2>&1 > /logs/install.log' \;
+find /build-deps \( -name "*.txz" -o -name "*.tbz" -o -name "*.tlz" -o -name "*.tgz" \) -exec bash -c 'echo Installing "{}" && installpkg "{}" 2>&1 > /logs/install.log' \;
 echo
 echo '#####  Running Scripts  #####'
 find /build-deps -name "*.sh" -exec bash -c 'echo Running "{}" && /bin/bash "{}" 2>&1 > /logs/scripts.log' \;
