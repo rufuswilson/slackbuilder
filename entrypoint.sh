e2e94bbcd0744a493b57af75075943b04d952bcc
rufuswilson <marsou75@gmail.com>
2025-06-10 19:37:23 +0200

    Added support for logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 cd /build-deps
 echo
 echo '#####  Installing Packages  #####'
-find /build-deps -name "*.txz" -exec bash -c 'echo Installing "{}" && installpkg "{}" 2>&1 > /dev/null' \;
+find /build-deps -name "*.txz" -exec bash -c 'echo Installing "{}" && installpkg "{}" 2>&1 > /logs/install.log' \;
 echo
 echo '#####  Running Scripts  #####'
-find /build-deps -name "*.sh" -exec bash -c 'echo Running "{}" && /bin/bash "{}"' \;
+find /build-deps -name "*.sh" -exec bash -c 'echo Running "{}" && /bin/bash "{}" 2>&1 > /logs/scripts.log' \;
 
 # Build package
 cd /slackbuild
 echo
 echo '#####  Running SlackBuild  #####'
-find /slackbuild -name "*.SlackBuild" -exec bash -c '/bin/bash "{}"' \; -quit
+find /slackbuild -name "*.SlackBuild" -exec bash -c '/bin/bash "{}" 2>&1 > /logs/build.log' \; -quit
