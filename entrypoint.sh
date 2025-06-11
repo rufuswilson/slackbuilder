17a0d7b78a38429a22a3e54d7d52233238b3de06
Lanjelin <6988319+Lanjelin@users.noreply.github.com>
2025-06-11 08:25:08 +0200

    Update entrypoint.sh
    Retain console output by using tee

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,29 @@
+#!/bin/bash
+
 # Install builder packages
 cd /build-deps
 echo
 echo '#####  Installing Packages  #####'
-find /build-deps \( -name "*.txz" -o -name "*.tbz" -o -name "*.tlz" -o -name "*.tgz" \) -exec bash -c 'echo Installing "{}" && installpkg "{}" 2>&1 > /logs/install.log' \;
+find /build-deps \( -name "*.txz" -o -name "*.tgz" -o -name "*.tlz" -o -name "*.tbz" \) -exec bash -c '
+  echo | tee -a /logs/install.log
+  echo Installing "{}" | tee -a /logs/install.log
+  installpkg "{}" 2>&1 | tee -a /logs/install.log
+' \;
+
+# Run scripts
 echo
 echo '#####  Running Scripts  #####'
-find /build-deps -name "*.sh" -exec bash -c 'echo Running "{}" && /bin/bash "{}" 2>&1 > /logs/scripts.log' \;
+find /build-deps -name "*.sh" -exec bash -c '
+  echo | tee -a /logs/scripts.log
+  echo Running "{}" | tee -a /logs/scripts.log
+  /bin/bash "{}" 2>&1 | tee -a /logs/scripts.log
+' \;
 
-# Build package
+# Run SlackBuild
 cd /slackbuild
 echo
 echo '#####  Running SlackBuild  #####'
-find /slackbuild -name "*.SlackBuild" -exec bash -c '/bin/bash "{}" 2>&1 > /logs/build.log' \; -quit
+find /slackbuild -name "*.SlackBuild" -exec bash -c '
+  echo Running "{}" | tee -a /logs/build.log
+  /bin/bash "{}" 2>&1 | tee -a /logs/build.log
+' \; -quit
