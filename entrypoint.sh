e09a1402d48bbd1253c53f5f0f2709b993798983
Lanjelin <6988319+Lanjelin@users.noreply.github.com>
2025-06-11 10:25:35 +0200

    variable LOGONLY
    Variable LOGONLY to prevent console output
    Running the commands directly
    https://github.com/Lanjelin/slackbuilder/issues/3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+# Handle loging
+log_output() {
+  local logfile="$1"
+  if [[ -n "$LOGONLY" ]]; then
+    cat >> "/logs/$logfile"
+  else
+    tee -a "/logs/$logfile"
+  fi
+}
+
 # Install builder packages
 cd /build-deps
-echo
-echo '#####  Installing Packages  #####'
-find /build-deps \( -name "*.txz" -o -name "*.tgz" -o -name "*.tlz" -o -name "*.tbz" \) -exec bash -c '
-  echo | tee -a /logs/install.log
-  echo Installing "{}" | tee -a /logs/install.log
-  installpkg "{}" 2>&1 | tee -a /logs/install.log
-' \;
+echo | log_output install.log
+echo '#####  Installing Packages  #####' | log_output install.log
+
+find /build-deps -type f \( -name "*.txz" -o -name "*.tgz" -o -name "*.tlz" -o -name "*.tbz" \) | while read -r pkg; do
+  echo | log_output install.log
+  echo Installing "$pkg" | log_output install.log
+  installpkg "$pkg" 2>&1 | log_output install.log
+done
 
 # Run scripts
-echo
-echo '#####  Running Scripts  #####'
-find /build-deps -name "*.sh" -exec bash -c '
-  echo | tee -a /logs/scripts.log
-  echo Running "{}" | tee -a /logs/scripts.log
-  /bin/bash "{}" 2>&1 | tee -a /logs/scripts.log
-' \;
+echo | log_output scripts.log
+echo '#####  Running Scripts  #####' | log_output scripts.log
+find /build-deps -type f -name "*.sh" | while read -r script; do
+  echo | log_output scripts.log
+  echo Running "$script" | log_output scripts.log
+  /bin/bash "$script" 2>&1 | log_output scripts.log
+done
 
 # Run SlackBuild
 cd /slackbuild
-echo
-echo '#####  Running SlackBuild  #####'
-find /slackbuild -name "*.SlackBuild" -exec bash -c '
-  echo Running "{}" | tee -a /logs/build.log
-  /bin/bash "{}" 2>&1 | tee -a /logs/build.log
-' \; -quit
+echo | log_output build.log
+echo '#####  Running SlackBuild  #####' | log_output build.log
+find /slackbuild -type f -name "*.SlackBuild" | while read -r sb; do
+  echo | log_output build.log
+  echo Running "$sb" | log_output build.log
+  /bin/bash "$sb" 2>&1 | log_output build.log
+  break
+done
