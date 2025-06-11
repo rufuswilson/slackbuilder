533e8d3f621371a6dd9c3233129502d24894e56f
rufuswilson <marsou75@gmail.com>
2025-06-11 23:48:00 +0200

    Changed LOGONLY to LOGLEVEL
    I tested the LOGONLY feature and was completely blocked as the process stopped somewhere because of a typo in my slack build.
    I thought it could be a good idea to add a bit of granularity to the amount of information printed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 # Handle loging
 log_output() {
-  local logfile="$1"
-  if [[ -n "$LOGONLY" ]]; then
+  local lvl="$1"
+  local logfile="$2"
+  if [[ -n "$LOGLEVEL" ]] && [[ $lvl -gt $LOGLEVEL ]]; then
     cat >> "/logs/$logfile"
   else
     tee -a "/logs/$logfile"
@@ -12,31 +13,30 @@ log_output() {
 
 # Install builder packages
 cd /build-deps
-echo | log_output install.log
-echo '#####  Installing Packages  #####' | log_output install.log
-
+echo | log_output 1 install.log
+echo '#####  Installing Packages  #####' '' | log_output 1 install.log
 find /build-deps -type f \( -name "*.txz" -o -name "*.tgz" -o -name "*.tlz" -o -name "*.tbz" \) | while read -r pkg; do
-  echo | log_output install.log
-  echo Installing "$pkg" | log_output install.log
-  installpkg "$pkg" 2>&1 | log_output install.log
+  echo | log_output 2 install.log
+  echo "*** $pkg" | log_output 1 install.log
+  installpkg "$pkg" 2>&1 | log_output 2 install.log
 done
 
 # Run scripts
-echo | log_output scripts.log
-echo '#####  Running Scripts  #####' | log_output scripts.log
+echo | log_output 1 scripts.log
+echo '#####  Running Scripts  #####' | log_output 1 scripts.log
 find /build-deps -type f -name "*.sh" | while read -r script; do
-  echo | log_output scripts.log
-  echo Running "$script" | log_output scripts.log
-  /bin/bash "$script" 2>&1 | log_output scripts.log
+  echo | log_output 2 scripts.log
+  echo "*** $script" | log_output 1 scripts.log
+  /bin/bash "$script" 2>&1 | log_output 2 scripts.log
 done
 
 # Run SlackBuild
 cd /slackbuild
-echo | log_output build.log
-echo '#####  Running SlackBuild  #####' | log_output build.log
+echo | log_output 1 build.log
+echo '#####  Running SlackBuild  #####' | log_output 1 build.log
 find /slackbuild -type f -name "*.SlackBuild" | while read -r sb; do
-  echo | log_output build.log
-  echo Running "$sb" | log_output build.log
-  /bin/bash "$sb" 2>&1 | log_output build.log
+  echo | log_output 2 build.log
+  echo "*** $sb" | log_output 1 build.log
+  /bin/bash "$sb" 2>&1 | log_output 2 build.log
   break
 done
